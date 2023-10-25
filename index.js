4767378564bc46a4426b59b9cb4e11384d851b48
Ridwan Adebosin <adebosinadewale@gmail.com>
2023-10-25 20:51:37 +0100

    JavaScript added

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const btn = document.getElementById("btn");
 const input = document.getElementById("birthday");
+const resultEl = document.getElementById("result");
 
-const CalculateAge = () => {
+const calculateAge = () => {
+  //getting the input information
   const birthdayValue = input.value;
   if (birthdayValue === "") {
     alert("Please enter your bithday value");
   } else {
     const age = getAge(birthdayValue);
+    resultEl.innerText = `Your age is ${age} ${age > 1 ? "years" : "year"} old`;
   }
 };
 
-const getAge = () => {
+const getAge = (birthdayValue) => {
   const currentDate = new Date();
-  console.log(currentDate);
+  const birthdayDate = new Date(birthdayValue);
+  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+  const month = currentDate.getMonth() - birthdayDate.getMonth();
+
+  if (
+    month < 0 ||
+    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
 };
 
-btn.addEventListener("click", CalculateAge);
+btn.addEventListener("click", calculateAge);
 
-// input.addEventListener();
+// input.addEventListener(CalculateAge, Age);
